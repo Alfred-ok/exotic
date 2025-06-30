b43beadd609207da14a5f12ed24605d515e10b80
Alfred-ok <okwirialfred@gmail.com>
2025-06-30 15:48:23 +0300

    user16

--- src/views/Reports/PaymentsTable.jsx
+++ src/views/Reports/PaymentsTable.jsx
@@ -273,19 +273,19 @@ export default function PaymentsTable() {
                       <Box
                         sx={{
                           backgroundColor:
-                            pay.product == 'vip'
+                            pay.product == 'Vip'
                               ? '#e3f2fd'
-                              : pay.product == 'premium'
+                              : pay.product == 'Premium'
                               ? '#f3e5f5'
-                              : pay.product == 'basic'
+                              : pay.product == 'Basic'
                               ? '#e8f5e9'
                               : '#e0e0e0',
                           color:
-                            pay.product == 'vip'
+                            pay.product == 'Vip'
                               ? '#0d47a1'
-                              : pay.product == 'premium'
+                              : pay.product == 'Premium'
                               ? '#6a1b9a'
-                              : pay.product == 'basic'
+                              : pay.product == 'Basic'
                               ? '#2e7d32'
                               : '#333',
                           px: 1.5,
