ed05ddd1c45cf554583755892e4493c7b5298e6a
Alfred-ok <okwirialfred@gmail.com>
2025-08-14 16:32:05 +0300

    overal payment5

--- src/menu-items/platformSelector.js
+++ src/menu-items/platformSelector.js
@@ -3,30 +3,45 @@ import { IconDashboard } from '@tabler/icons-react';
 // constant
 const icons = { IconDashboard };
 
+// get role from localStorage
+const role = localStorage.getItem('userRole'); // 'admin', 'sub-admin', 'sales'
+
 // ==============================|| DASHBOARD MENU ITEMS ||============================== //
 
 const platformSelector = {
   id: 'Platform Selcetor',
   title: 'Platform Selcetor',
   type: 'group',
-  children: [
-    {
-      id: 'default',
-      title: 'Platform Selector',
-      type: 'item',
-      url: '/platform-selector',
-      icon: icons.IconDashboard,
-      breadcrumbs: false
-    },
-    {
-      id: 'Overal Payment',
-      title: 'Overal Payment',
-      type: 'item',
-      url: '/overal-payment',
-      icon: icons.IconDashboard,
-      breadcrumbs: false
-    }
-  ]
+  children:
+    role === 'admin'
+      ? [
+          {
+            id: 'default',
+            title: 'Platform Selector',
+            type: 'item',
+            url: '/platform-selector',
+            icon: icons.IconDashboard,
+            breadcrumbs: false
+          },
+          {
+            id: 'Overal Payment',
+            title: 'Overal Payment',
+            type: 'item',
+            url: '/overal-payment',
+            icon: icons.IconDashboard,
+            breadcrumbs: false
+          }
+        ]
+      : [
+          {
+            id: 'default',
+            title: 'Platform Selector',
+            type: 'item',
+            url: '/platform-selector',
+            icon: icons.IconDashboard,
+            breadcrumbs: false
+          }
+        ]
 };
 
 export default platformSelector;
