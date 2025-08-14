48ca59f055714acc5ad61c4244454a95ca9a6118
Alfred-ok <okwirialfred@gmail.com>
2025-08-14 15:38:13 +0300

    overal payment2

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -6,13 +6,13 @@ import platformSelector from './platformSelector';
 import dividlogo from './dividlogo';
 import administrator from './Administrator';
 import Report from './Report';
-import OveralPayment from '../views/Reports/OveralPayment';
+import OveralPayment from './overalpayment';
 
 
 // ==============================|| MENU ITEMS ||============================== //
 //Change
 const menuItems = {
-  items: [dividlogo, platformSelector, dashboard, administrator, OveralPayment].filter(Boolean)
+  items: [dividlogo, platformSelector,OveralPayment, dashboard, administrator].filter(Boolean)
 };
 
 export default menuItems;
