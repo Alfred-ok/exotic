964bcd581dc81ba15879e7fc79e42c7ef554cc1b
Alfred <alfred.okwiri@swizzsoft.com>
2025-10-23 10:28:01 +0300

    update

--- src/layout/MainLayout/Header/index.jsx
+++ src/layout/MainLayout/Header/index.jsx
@@ -24,6 +24,7 @@ export default function Header() {
   const { menuMaster } = useGetMenuMaster();
   const drawerOpen = menuMaster.isDashboardDrawerOpened;
 
+  const username = localStorage.getItem('userName');
   return (
     <>
       {/* logo & toggler button */}
@@ -51,7 +52,7 @@ export default function Header() {
           <IconMenu2 stroke={1.5} size="20px" />
         </Avatar>
       </Box>
-
+      <Box><h2>{username && username}</h2></Box>
       {/* header search */}
       {/*<SearchSection />*/}
       <Box sx={{ flexGrow: 1 }} />
